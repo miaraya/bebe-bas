6abeef448a82bbf953bae45f5862658ddeff3155
Miguel Araya <miaraya@gmail.com>
2018-12-04 16:44:54 +0700

    prod

--- src/containers/constants.js
+++ src/containers/constants.js
@@ -1,14 +1,14 @@
-export const api = 'http://localhost:5000/api/'
-//export const api = 'http://apibas.herokuapp.com/api/'
+//export const api = 'http://localhost:5000/api/'
+export const api = "http://apibas.herokuapp.com/api/";
 
 export const garment_options =
-  api + 'containers/bebe-garment-options/download/'
-export const fabric_url = api + 'containers/bebe-fabrics-thumbnails/download/'
-export const fabric_url_full = api + 'containers/bebe-fabrics/download/'
+  api + "containers/bebe-garment-options/download/";
+export const fabric_url = api + "containers/bebe-fabrics-thumbnails/download/";
+export const fabric_url_full = api + "containers/bebe-fabrics/download/";
 
-export const item_images = api + 'containers/bebe-order-images/download/'
+export const item_images = api + "containers/bebe-order-images/download/";
 
-export const location_url = api + 'containers/bebe-locations/download/'
+export const location_url = api + "containers/bebe-locations/download/";
 
-export const swatchbook_url = ''
-export const login_url = api + 'login'
+export const swatchbook_url = "";
+export const login_url = api + "login";
