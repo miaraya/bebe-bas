002a14d5a351f445bf2d44ce7c93addd8abd1990
Miguel Araya <miaraya@gmail.com>
2019-11-29 15:20:44 +0700

    route collection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,28 @@
 import React from "react";
-import {Route, IndexRoute} from "react-router";
-import Home from "../containers/home";
-import Item from "../containers/item";
-import Itemprint from "../containers/itemprint";
-import Order from "../containers/order";
-import Fabric from "../containers/fabric";
-import Swatchbook from "../containers/swatchbook";
-import _Search from "../containers/search";
-import Inventory from "../containers/inventory";
-import Report from "../containers/report";
+import {Route, IndexRoute} from "react-router"
+import Home from "../containers/home"
+import Item from "../containers/item"
+import Itemprint from "../containers/itemprint"
+import Order from "../containers/order"
+import Fabric from "../containers/fabric"
+import Swatchbook from "../containers/swatchbook"
+import _Search from "../containers/search"
+import Inventory from "../containers/inventory"
+import Report from "../containers/report"
+import Dashboard from "../containers/dashboard"
+import Collection from "../containers/collection"
+
+
 
 import AuthService from "../AuthService";
 const Auth = new AuthService(null);
 
 function loggedIn() {
-  return Auth.loggedIn();
+	return Auth.loggedIn()
 }
 
 function requireAuth(nextState, replace) {
-  if (!loggedIn()) {
+	if (!loggedIn()) {
     replace({
       pathname: "/"
     });
@@ -30,7 +34,7 @@ const createRoutes = () => {
       <IndexRoute component={Home} />
       <Route path={"/search"} component={_Search} />
 
-      <Route path={"/i"} component={Home} />
+			<Route path={"/i"} component={Home} />
       <Route path={"/i/:id"} component={Item} />
       <Route path={"/i/print/:id"} component={Itemprint} />
       <Route path={"/o/:id"} component={Order} />
@@ -40,6 +44,10 @@ const createRoutes = () => {
       <Route path={"/s/:id"} component={Swatchbook} />
       <Route path={"/inventory"} component={Inventory} onEnter={requireAuth} />
       <Route path={"/reports"} component={Report} />
+      <Route path={"/dashboard"} component={Dashboard} />
+      <Route path={"/c/:id"} component={Collection} />
+
+
 
       <Route path={"/swatchbooks/swatchbook/:id"} component={Swatchbook} />
       <Route path={"/fabrics/fabric/:id"} component={Fabric} />
@@ -47,6 +55,6 @@ const createRoutes = () => {
   );
 };
 
-const Routes = createRoutes();
+const Routes = createRoutes()
 
-export default Routes;
+export default Routes
