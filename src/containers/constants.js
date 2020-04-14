d5bf8e7a83583aa6cd986e414afb569980a9502f
Miguel Araya <miaraya@gmail.com>
2020-04-14 16:38:39 +0700

    login url

--- src/containers/constants.js
+++ src/containers/constants.js
@@ -1,39 +1,39 @@
-//export const api = "http://localhost:5000/api/"
+//export const api = "http://192.168.1.200:5000/api/";
 
-export const api = "http://apibas.herokuapp.com/api/"
+export const api = "http://apibas.herokuapp.com/api/";
 
 export const garment_options =
-  api + "containers/bebe-garment-options/download/"
-export const fabric_url = api + "containers/bebe-fabrics-thumbnails/download/"
-export const fabric_url_full = api + "containers/bebe-fabrics/download/"
+  api + "containers/bebe-garment-options/download/";
+export const fabric_url = api + "containers/bebe-fabrics-thumbnails/download/";
+export const fabric_url_full = api + "containers/bebe-fabrics/download/";
 
-export const item_images = api + "containers/bebe-order-images/download/"
+export const item_images = api + "containers/bebe-order-images/download/";
 
-export const location_url = api + "containers/bebe-locations/download/"
+export const location_url = api + "containers/bebe-locations/download/";
 
-export const swatchbook_url = ""
-export const login_url = api + "login"
+export const swatchbook_url = "";
+export const login_url = api + "users/login";
 
 export const formItemLayout = {
-	labelCol: {
-		xs: {span: 24},
-		sm: {span: 8}
-	},
-	wrapperCol: {
-		xs: {span: 24},
-		sm: {span: 16}
-	}
-}
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
 
 export const tailFormItemLayout = {
-	wrapperCol: {
-		xs: {
-			span: 24,
-			offset: 0
-		},
-		sm: {
-			span: 16,
-			offset: 8
-		}
-	}
-}
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
